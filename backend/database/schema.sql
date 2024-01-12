1d8aeb08ce1a92802d2fb7c376abfb6273b29018
Clément Féménias--Noir <femenias.c@gmail.com>
2024-01-12 17:23:52 +0100

    bdd role + user

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,19 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
+CREATE TABLE `role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO role (name) values ('ADMIN'), ('USER');
+
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `email` varchar(75) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `image` VARCHAR(255), 
+  `id_role` INT,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`id_role`) REFERENCES role(id)
 );
